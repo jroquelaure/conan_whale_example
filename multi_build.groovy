6b2de63e95f0ff2f69568b73e6f3b591acc0e44c
jroquelaure <jonathanr@jfrog.com>
2018-07-04 15:40:30 +0200

    working dirty version

--- multi_build.groovy
+++ multi_build.groovy
@@ -2,16 +2,45 @@ import groovy.json.JsonSlurperClassic
 
 def get_conan_images()
 {
-    
 
-    def aqlCmd = "curl -uadmin:password -X POST http://localhost:8081/artifactory/api/search/aql -T /Users/jon/workspace/conan/docker-multibuild/conan-demo/getConanBuilderImages.aql"
+    def aqlCmd = "curl -uadmin:password -X POST http://localhost:8081/artifactory/api/search/aql -T /Users/jon/workspace/conan/docker-multibuild/conan_whale_example/getConanBuilderImages.aql"
     def imagesPath = aqlCmd.execute().text
     def jsonSlurper = new JsonSlurperClassic()
     def images = jsonSlurper.parseText("${imagesPath}")
 
+   
     return images.results
 }
 
+def launch_task_group(imageNames, conf_repo_branch, conf_repo_url,user_channel,name_version)
+{
+    tasks = [:]
+    for(int i=0; i<imageNames.path.size(); i++){
+             def imageName = imageNames.path[i]
+             sh "echo $imageName"
+             tasks[imageName] = { -> build(job: "conan-multi-task2",
+                              parameters: [
+                                 string(name: "image", value: imageName),
+                                 string(name: "name_version", value: name_version),
+                                 string(name: "user_channel", value: user_channel),
+                                 string(name: "conf_repo_url", value: conf_repo_url),
+                                 string(name: "conf_repo_branch", value: conf_repo_branch)
+                              ]
+                       )
+            }
+        }
+       
+            
+        parallel(tasks)
+}
+
+properties([parameters([string(description: 'Recipe reference that has changed', name: 'name_version', defaultValue: 'LIB_A/1.0'),
+                        string(description: 'User/Channel', name: 'user_channel', defaultValue: 'lasote/stable'),
+                        string(description: 'Config repository URL', name: 'conf_repo_url', defaultValue: 'https://github.com/jroquelaure/skynet_example.git'),
+                        string(description: 'Config repository branch', name: 'conf_repo_branch', defaultValue: 'master')
+                        ])])
+
+@NonCPS def entries(m) {m.collect {k, v -> [k, v]}}
 
 node{
     def imageNames
@@ -29,24 +58,12 @@ node{
     stage("Run builds in parallel for environment")
     {
         //foreach docker image for conan from RTF build job (current docker image) _
+        //first step simple build
         tasks = [:]
         imageNames = get_conan_images()
-        for(int i=0; i<imageNames.path.size(); i++){
-             image = imageNames.path[i]
-             sh "echo $image"
-             tasks[image] = { -> build(job: "conan-single-test",
-                              parameters: [
-                                 string(name: "image", value: image)
-                              ]
-                       )
-            }
-        }
-        parallel(tasks)
+        launch_task_group(imageNames,conf_repo_branch, conf_repo_url,user_channel,name_version)
+        
         //final the multi-build -> parrallel per profiles per environment
-        //Need to link conan profile to docker image with metadata in order to execute build order command for each profiles
-        
-        //then parrallelize per image -> build order workflow
-        
         //Warn : final need merge of PR from nico (dir with docker image in jenkins does not work)
 
     }
